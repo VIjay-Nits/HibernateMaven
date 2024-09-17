f3997b20ecde4b80f628c8b4cd95057c2a784f28
VIjay-Nits <vijaygupta131999@gmail.com>
2024-09-18 02:43:55 +0530

    configure hibernate cfg.xml file

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,5 +1,8 @@
 package com;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        System.out.println(factory);
+
     }
 }
